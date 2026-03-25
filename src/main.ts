ba85eadf5679d33c631fd93ded43edf671d9d558
Álvaro Cotes <alvarocotes.jc@gmail.com>
2026-03-25 13:55:18 -0500

    Enhance CORS configuration in main.ts to allow all origins and specify allowed methods and headers.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,12 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: '*',
+    exposedHeaders: '*',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
